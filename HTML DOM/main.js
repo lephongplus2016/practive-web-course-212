71bfe45a298a7628010169b83284b1669ee2ecac
lephongplus2016 <lephongplus2016@gmail.com>
2022-03-11 09:12:18 +0700

    learn DOM html

---
+++ HTML DOM/main.js
@@ -0,0 +1,8 @@
+document.write("HELLO PHONG LE");
+
+let heading = document.getElementById("heading");
+
+// chú ý heading là 1 node kiểu element, chứ không phải là 1 dòng code
+console.log({
+    element: heading,
+});
